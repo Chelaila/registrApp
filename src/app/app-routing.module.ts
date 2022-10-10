55b4f2e41ae9ee5c5335bfc74715a5237af9b44a
omGuerraAlfaro <om.guerra@duocuc.cl>
2022-10-10 16:12:40 -0300

    form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },  
   {
     path: '',
-    redirectTo: 'home/asistencia',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
-
 import { LoginPageRoutingModule } from './login-routing.module';
-
 import { LoginPage } from './login.page';
+
+//form
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,    
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
-  declarations: [LoginPage, ]
+  declarations: [LoginPage,]
 })
 export class LoginPageModule {}
